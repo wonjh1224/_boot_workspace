1ae3ecfd95ac5f2e69a178fca7c75a6477b3b358
wonjh1224 <candle147@naver.com>
2024-01-22 15:11:56 +0900

    first thymeleaf

---
+++ src/main/java/com/example/thymeleaf/domain/SampleDTO.java
@@ -0,0 +1,14 @@
+package com.example.thymeleaf.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter @Setter @ToString
+public class SampleDTO {
+
+	private String p1, p2, p3;
+	
+}
